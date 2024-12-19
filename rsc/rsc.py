7aacf3f0b866e03d02fa11cb0376de3c17d02f89
Seohyeong <jseoh95@gmail.com>
2024-12-20 08:09:11 +1100

    add to_graph()

--- rsc/rsc.py
+++ rsc/rsc.py
@@ -17,6 +17,22 @@ class Matrix:
     def to_list(self) -> list[list[str]]:
         return self.grid
     
+    def to_graph(self) -> dict[tuple[int, int]: list[tuple[int, int]]]:
+        graph = {}
+        for i in range(1, self.num_row - 1):
+            for j in range(1, self.num_col - 1):
+                if self.grid[i][j] != '#':
+                    graph[(i, j)] = []
+                    if self.grid[i - 1][j] != '#':
+                        graph[(i, j)].append((i - 1, j))
+                    if self.grid[i + 1][j] != '#':
+                        graph[(i, j)].append((i + 1, j))
+                    if self.grid[i][j - 1] != '#':
+                        graph[(i, j)].append((i, j - 1))
+                    if self.grid[i][j + 1] != '#':
+                        graph[(i, j)].append((i, j + 1))
+        return graph
+                        
     def get_loc(self, item: str) -> tuple[int, int]:
         for i, row in enumerate(self.grid):
             for j, char in enumerate(row):
