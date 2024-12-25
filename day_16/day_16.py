fff4c6fee9bfc0a0e3a250d31b6f99be14fcbd83
Seohyeong <jseoh95@gmail.com>
2024-12-26 10:31:15 +1100

    use priority queue for getting the min node

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -3,6 +3,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 from collections import deque
+import heapq
 
 from rsc.rsc import Reader, Matrix
 from rsc.rsc import prettyprint
@@ -19,39 +20,7 @@ e_loc = grid_builder.get_loc('E')
 graph = grid_builder.to_graph()
 
 
-def bfs(graph, s_loc, e_loc):
-    # TODO: bfs giving priority to the direction it's currently on
-    queue = deque([[s_loc]]) # keep list of paths
-    while queue:
-        path = queue.popleft()
-        node = path[-1]
-        if node == e_loc:
-            return path
-        for adj in graph.get(node, []):
-            if adj not in path:
-                new_path = list(path)
-                new_path.append(adj)
-                queue.append(new_path)
-
-# path = bfs(graph, s_loc, e_loc)
-
-# for loc in path:
-#     grid[loc[0]][loc[1]] = '>'
-    
-# prettyprint(grid)
-
-def get_min_node(distances, visited):
-    min_node = None
-    min_node_dist = float('inf')
-    for node in distances:
-        if node not in visited:
-            if distances[node][0] <= min_node_dist:
-                min_node = node
-                min_node_dist = distances[node][0]
-    return min_node
-
 def dijkstra(graph, s_loc, with_path = False):
-    # delta = {'E': (0, 1), 'W': (0, -1), 'N': (-1, 0), 'S': (1, 0)}
     delta = {(0, 1): 'E', (0, -1): 'W', (-1, 0): 'N', (1, 0): 'S'}
     
     if with_path:
@@ -61,33 +30,27 @@ def dijkstra(graph, s_loc, with_path = False):
         distances = {node: (float('inf'), None) for node in graph} # score, dir
         distances[s_loc] = (0, 'E')
     
-    visited = set()
-    visited.add(s_loc)
+    q = []
+    heapq.heappush(q, (0, s_loc))
     
-    # start with the s_loc
-    for nb in graph[s_loc]:
-        curr_delta = (nb[0] - s_loc[0], nb[1] - s_loc[1])
-        s_to_nb = 1 if distances[s_loc][1] == delta[curr_delta] else 1001
-        if distances[s_loc][0] + s_to_nb < distances[nb][0]:
-            if with_path:
-                distances[nb] = (distances[s_loc][0] + s_to_nb, delta[curr_delta], distances[s_loc][2] + [nb])
-            else:
-                distances[nb] = (distances[s_loc][0] + s_to_nb, delta[curr_delta])
-            
-    # for rest of the nodes 
-    for _ in range(len(graph) - 1):
-        node = get_min_node(distances, visited)
-        visited.add(node)
+    while q:
+        dist, node = heapq.heappop(q)
+        node_dist, node_dir, *path = distances[node]
         
+        if node_dist < dist: # already visited
+            continue
+
         for nb in graph[node]:
-            curr_delta = (nb[0] - node[0], nb[1] - node[1])
-            node_to_nb = 1 if distances[node][1] == delta[curr_delta] else 1001
-            if distances[node][0] + node_to_nb < distances[nb][0]:
+            nb_delta = (nb[0] - node[0], nb[1] - node[1])
+            nb_dir = delta[nb_delta]
+            cost = (node_dist + 1) if node_dir == nb_dir else (node_dist + 1001)
+            if cost < distances[nb][0]:
+                heapq.heappush(q, (cost, nb))
                 if with_path:
-                    distances[nb] = (distances[node][0] + node_to_nb, delta[curr_delta], distances[node][2] + [nb])
+                    distances[nb] = (cost, nb_dir, path + [nb])
                 else:
-                    distances[nb] = (distances[node][0] + node_to_nb, delta[curr_delta])
-    
+                    distances[nb] = (cost, nb_dir)
+
     return distances
         
     
